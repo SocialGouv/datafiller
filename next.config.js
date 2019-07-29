a16655a78ea3a48878871afc9beccf53f477e836
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-07-29 09:54:05 +0200

    chore: update deps + add linter

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
   useFileSystemPublicRoutes: false,
   publicRuntimeConfig: {
     KINTO_URL: process.env.KINTO_URL || "http://127.0.0.1:8888/v1",
     PACKAGE_VERSION: require("./package.json").version,
     KINTO_BUCKET: "datasets"
   }
-};
+});
