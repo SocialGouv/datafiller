759ec311afd728525f05b35de35202884a62ee27
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-08-03 02:50:38 +0200

    feat: add sentry

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,63 @@
 import React from "react";
 import App, { Container } from "next/app";
+import getConfig from "next/config";
 import Head from "next/head";
+import * as Sentry from "@sentry/browser";
+import ErrorPage from "./_error";
 
 import KintoContext from "../src/kinto/KintoContext";
 import kintoClient from "../src/kinto/client";
 
+const {
+  publicRuntimeConfig: { SENTRY_PUBLIC_DSN }
+} = getConfig();
+
+console.log("SENTRY_PUBLIC_DSN", SENTRY_PUBLIC_DSN);
+
+if (typeof window !== "undefined" && SENTRY_PUBLIC_DSN) {
+  Sentry.init({ dsn: SENTRY_PUBLIC_DSN, debug: true });
+}
+
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      try {
+        pageProps = await Component.getInitialProps(ctx);
+      } catch (err) {
+        pageProps = { statusCode: 500, message: err.message };
+      }
+    }
+    // pageUrl and ogImage are only defined on serverside request
+    if (ctx.req) {
+      pageProps.pageUrl = `${ctx.req.protocol}://${ctx.req.headers.host}${
+        ctx.req.path
+      }`;
+      pageProps.ogImage = `${ctx.req.protocol}://${
+        ctx.req.headers.host
+      }/static/images/social-preview.png`;
+    }
+
+    return { pageProps };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    Sentry.withScope(scope => {
+      Object.keys(errorInfo).forEach(key => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+
+      Sentry.captureException(error);
+    });
+    super.componentDidCatch(error, errorInfo);
+  }
+
   render() {
     const { Component, pageProps } = this.props;
+    if (pageProps.statusCode) {
+      return <ErrorPage statusCode={pageProps.statusCode} />;
+    }
     return (
       <Container>
         <Head>
--- server.js
+++ server.js
@@ -16,24 +16,47 @@ const handle = app.getRequestHandler();
 
 const PORT = process.env.PORT || 3000;
 
+// const sourcemapsForSentryOnly = token => (req, res, next) => {
+//   // In production we only want to serve source maps for Sentry
+//   console.log("sourcemapsForSentryOnly", req.headers, process.env.NODE_ENV);
+//   if (
+//     !process.env.NODE_ENV !== "production" &&
+//     !!token &&
+//     req.headers["x-sentry-token"] !== token
+//   ) {
+//     res
+//       .status(401)
+//       .send(
+//         "Authentication access token is required to access the source map."
+//       );
+//     return;
+//   }
+//   next();
+// };
+
 app.prepare().then(() => {
   const server = express();
+  const { Sentry } = require("./sentry")(app.buildId);
 
-  // setup a kinto proxy
-  server.use(proxyMiddleware("/kinto", kintoProxy));
-
-  server.get("/api/ccn/:ccn.json", async (req, res) => {
-    if (req.params.ccn.match(/^KALICONT/)) {
-      const ccn = require(`@socialgouv/kali-data/data/${req.params.ccn}.json`);
-      res.json(ccn);
-    }
-    res.status(404).end();
-  });
-
-  server.get("*", (req, res) => {
-    console.log("server handler", req.url);
-    return handle(req, res);
-  });
+  server
+    .use(Sentry.Handlers.requestHandler())
+    //  .get(/\.map$/, sourcemapsForSentryOnly(process.env.SENTRY_TOKEN))
+    // setup a kinto proxy
+    .use(proxyMiddleware("/kinto", kintoProxy))
+    .get("/api/ccn/:ccn.json", async (req, res) => {
+      if (req.params.ccn.match(/^KALICONT/)) {
+        const ccn = require(`@socialgouv/kali-data/data/${
+          req.params.ccn
+        }.json`);
+        res.json(ccn);
+      }
+      res.status(404).end();
+    })
+    .get("*", (req, res) => {
+      console.log("server handler", req.url);
+      return handle(req, res);
+    })
+    .use(Sentry.Handlers.errorHandler());
 
   server.listen(PORT, err => {
     if (err) {
