93b4ed5632c5c47443c04ed2cd4aa8a37bd55600
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-11-18 16:11:57 +0100

    fix: use new API format

--- src/cdtn-api.js
+++ src/cdtn-api.js
@@ -5,16 +5,34 @@ const API_URL =
 //"https://cdtn-api.num.social.gouv.fr/api/v1";
 //"https://api.code-du-travail-numerique.incubateur.social.gouv.fr/api/v1";
 
+const sortByRelevance = (a, b) => {
+  if (a._source._score < b._source._score) {
+    return 1;
+  } else if (a._source._score > b._source._score) {
+    return -1;
+  }
+  return 0;
+};
+
 const fetchResults = endpoint => (query = "", excludeSources = "") => {
   const url = `${API_URL}/${endpoint}?q=${encodeURIComponent(
     query
   )}&excludeSources=${encodeURIComponent(excludeSources)}&size=25`;
-  return fetch(url).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error("Un problème est survenu.");
-  });
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Un problème est survenu.");
+    })
+    .then(data => {
+      return [...data.documents, ...data.articles, ...data.themes]
+        .map(result => ({
+          _source: result
+        }))
+        .sort(sortByRelevance)
+        .slice(0, 25);
+    });
 };
 const searchResults = fetchResults("search");
 const suggestResults = fetchResults("suggest");
