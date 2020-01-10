b7c7b7f199e60f87b1e746cf2dc3f6313cea24f8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-01-10 09:35:27 +0100

    fix: use latest master dump

--- scripts/download-dump.sh
+++ scripts/download-dump.sh
@@ -3,7 +3,7 @@
 # download master version of CDTN data
 
 #`curl https://api.github.com/repos/SocialGouv/code-du-travail-numerique/branches/master | jq -r ".commit.sha" -`
-SHA=8582d243283d0614f8d8f40ac3e5dfebf62c28c6
+SHA=fb4a8e162ca2d4265d741c77e480c3a904b381c4
 
 CDTN_REGISTRY=registry.gitlab.factory.social.gouv.fr/socialgouv/code-du-travail-numerique
 
