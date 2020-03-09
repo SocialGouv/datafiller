d0961ecf460bf43ebb85bc6ce9cd37482c0f1c22
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-03-09 02:02:16 +0100

    fix reorder requetes script

--- scripts/fix-requetes-positions.js
+++ scripts/fix-requetes-positions.js
@@ -47,49 +47,10 @@ const sortRefs = (a, b) => {
   return 0;
 };
 
-const refs1 = [
-  {
-    url: "",
-    position: 5
-  },
-  { url: "", position: 3 },
-  {
-    url: "",
-    position: 0
-  },
-  {
-    url: "",
-    title: "pouet"
-  },
-  {
-    url: "/contribution/c3",
-    title: "c3",
-    relevance: 3
-  },
-  {
-    url: "/code-du-travail/xxx",
-    title: "pouet2",
-    relevance: 3
-  },
-  {
-    url: "/contribution/c1",
-    title: "c1",
-    relevance: 3
-  },
-  { url: "", position: 6 },
-  { url: "/extra", relevance: 5 }
-];
-
 const setNaturalPosition = (item, i) => ({
   ...item,
   position: i + 1
 });
-
-refs1.sort(sortRefs);
-
-console.log("refs1", refs1.map(setNaturalPosition));
-
-process.exit(0);
 
 const reorderPositions = refs =>
   refs.sort(sortRefs) && refs.map(setNaturalPosition);
@@ -98,32 +59,17 @@ const reorderPositions = refs =>
 const fixCollection = async collection => {
   const records = await getCollection(collection);
 
-  const validRecords = await pAll(
+  return await pAll(
     records
       .filter(req => req.refs && req.refs.length)
       .slice(0, 10)
-      .map(record => () => {
+      .map(record => async () => {
         const newRefs = reorderPositions(record.refs);
-
-        //console.log("io", record.refs, newRefs);
-
-        console.table(newRefs);
-        // return updateRecord(collection, record.id, { refs: newRefs }).then(
-        //   () => ({
-        //     ...record,
-        //     refs: newRefs
-        //   })
-        // );
+        // apply changes
+        return await updateRecord(collection, record.id, { refs: newRefs });
       }),
-    { concurrency: 5 }
+    { concurrency: 3 }
   );
-
-  //const allRefs = getAllRefs(validRecords);
-  // console.log(
-  //   `\n## ${collection} : ${allRefs.filter(r => !r.valid).length}/${
-  //     allRefs.length
-  //   } wrong references\n`
-  // );
 };
 
-fixCollection("requetes");
+fixCollection("requetes").catch(console.log);
