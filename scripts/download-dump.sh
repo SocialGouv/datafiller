f541f58e62da83983637f39a33a0e466e280fca8
Lionel <lionel@lumographe.fr>
2020-03-18 10:13:57 +0100

    chore: update data (#115)

--- scripts/download-dump.sh
+++ scripts/download-dump.sh
@@ -3,7 +3,7 @@
 # download master version of CDTN data
 
 #`curl https://api.github.com/repos/SocialGouv/code-du-travail-numerique/branches/master | jq -r ".commit.sha" -`
-SHA=6c4e2341af6b6ad74ba897e30e4854e43a7dfc1d
+SHA=9aa3600e20a122711043e8e039a892801f8486a8
 
 CDTN_REGISTRY=registry.gitlab.factory.social.gouv.fr/socialgouv/code-du-travail-numerique
 
